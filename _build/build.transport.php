90754cc64fb4516c425d995323d10f4b34fba2b3
Shaun McCormick <shaun@modx.com>
2012-02-07 17:01:42 -0600

    Update to 1.4

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -28,7 +28,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','Articles');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','1.4.3');
+define('PKG_VERSION','1.4.4');
 define('PKG_RELEASE','pl');
 
 /* define sources */
