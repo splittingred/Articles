73260a12724fa773ca27bdafbdfe45dc72ff796a
Shaun McCormick <shaun@modx.com>
2012-07-04 22:48:13 -0500

    Update getPage to 1.2.3-pl

--- _build/data/transport.subpackages.php
+++ _build/data/transport.subpackages.php
@@ -29,7 +29,7 @@
  */
 $subpackages = array(
     'archivist'      => 'archivist-1.2.3-pl',
-    'getpage'        => 'getpage-1.2.2-pl',
+    'getpage'        => 'getpage-1.2.3-pl',
     'getresources'   => 'getresources-1.5.0-pl',
     'quip'           => 'quip-2.3.1-pl',
     'taglister'      => 'taglister-1.1.7-pl',
