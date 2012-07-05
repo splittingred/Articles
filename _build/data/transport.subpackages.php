3438e5a19f5c6e442246dd6db49f4a7126c8e9a1
Shaun McCormick <shaun@modx.com>
2012-07-04 22:23:02 -0500

    Update getResources to 1.5.0-pl

--- _build/data/transport.subpackages.php
+++ _build/data/transport.subpackages.php
@@ -30,7 +30,7 @@
 $subpackages = array(
     'archivist'      => 'archivist-1.2.3-pl',
     'getpage'        => 'getpage-1.2.2-pl',
-    'getresources'   => 'getresources-1.4.2-pl',
+    'getresources'   => 'getresources-1.5.0-pl',
     'quip'           => 'quip-2.3.1-pl',
     'taglister'      => 'taglister-1.1.7-pl',
 );
