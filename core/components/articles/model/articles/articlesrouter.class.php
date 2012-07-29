86563b46815c06769201fcdde5a4f9575fd84980
Daniel Schlessmann <daniel@dschlessmann.com>
2012-07-30 00:08:32 +0200

    Invalid URL parameters should lead to an error instead of passing them on to Archivist

--- core/components/articles/model/articles/articlesrouter.class.php
+++ core/components/articles/model/articles/articlesrouter.class.php
@@ -85,9 +85,25 @@ class ArticlesRouter {
         /* normal yyyy/mm/dd or yyyy/mm */
         } else {
             /* set Archivist parameters for date-based archives */
-            $_GET[$prefix.'year'] = $params[0];
-            if (isset($params[1])) $_GET[$prefix.'month'] = $params[1];
-            if (isset($params[2])) $_GET[$prefix.'day'] = $params[2];
+            if(preg_match('/^\d+$/', $params[0]) === 1) {
+                $_GET[$prefix.'year'] = $params[0];
+                if(isset($params[1])) {
+                    if(preg_match('/^\d+$/', $params[1]) === 1) {
+                        $_GET[$prefix.'month'] = $params[1];
+                        if(isset($params[2])) {
+                            if(preg_match('/^\d+$/', $params[2]) === 1) {
+                                $_GET[$prefix.'day'] = $params[2];
+                            } else {
+                                return false;   // non-numerical value
+                            }
+                        }
+                    } else {
+                        return false; // non-numerical value
+                    }
+                }
+            } else {
+                return false; // non-numerical value
+            }
         }
 
         /* forward */
