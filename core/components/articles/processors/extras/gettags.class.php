d45b0c1b3c2fb73770ce2e31ac2471383b9dc406
Jan Peca <theboxercz@gmail.com>
2013-08-05 14:45:44 +0200

    Added SuperBoxSelect for tags

---
+++ core/components/articles/processors/extras/gettags.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package articles
+ * @subpackage processors
+ */
+class ArticleExtrasGetTagsProcessor extends modObjectGetListProcessor {
+    public $checkListPermission = true;
+
+
+    public function process() {
+        $container = $this->getProperty('container', false);
+        if(!$container){
+            return false;
+        }
+
+        $parent = $this->modx->getObject('modResource', $container);
+        if(!$parent){
+            return false;
+        }
+
+        $articles = $parent->getMany('Children');
+        $articleIDs = array();
+        foreach($articles as $article){
+            $articleIDs[] = $article->id;
+        }
+
+        $templateVariable = $this->modx->getObject('modTemplateVar', array('name' => 'articlestags'));
+        if(!$templateVariable){
+            return false;
+        }
+
+        $c = $this->modx->newQuery('modTemplateVarResource');
+        $c->where(array(
+                       'tmplvarid' => $templateVariable->id,
+                       'contentid:IN' => $articleIDs
+                  ));
+
+        $tagsObject = $this->modx->getCollection('modTemplateVarResource', $c);
+        $tags = array();
+
+        foreach($tagsObject as $tagObject){
+            $addTags = explode(',',$tagObject->value);
+            foreach($addTags as &$addTag){
+                $addTag = trim($addTag);
+            }
+            $tags = array_merge($tags, $addTags);
+        }
+
+        $tags = ArticlesService::arrayUnique($tags);
+
+        $returnArray = array();
+        foreach($tags as $tag){
+            $returnArray[] = array('tag' => $tag);
+        }
+
+        return $this->outputArray($returnArray, count($returnArray));
+    }
+
+}
+return 'ArticleExtrasGetTagsProcessor';
