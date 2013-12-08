54934d374951640191c05397ddabe4484b88b16e
JP DeVries <johnpaul.devries@gmail.com>
2013-12-08 11:22:05 -0800

    Bumping version to 1.7.6-dev

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -28,8 +28,8 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','Articles');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','1.7.5');
-define('PKG_RELEASE','pl');
+define('PKG_VERSION','1.7.6');
+define('PKG_RELEASE','dev');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
